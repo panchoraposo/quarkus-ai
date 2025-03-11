d534b4179957f35f8c74815768b748ada1206523
Francisco Raposo <fraposo@redhat.com>
2025-03-11 18:00:56 -0300

    chatbot

---
+++ deepseek-ai-chatbot/src/main/java/com/redhat/demo/CustomerSupportAgent.java
@@ -0,0 +1,19 @@
+package com.redhat.demo;
+
+import dev.langchain4j.service.SystemMessage;
+import io.quarkiverse.langchain4j.RegisterAiService;
+import io.smallrye.mutiny.Multi;
+import jakarta.enterprise.context.SessionScoped;
+
+@SessionScoped
+@RegisterAiService
+public interface CustomerSupportAgent {
+
+    @SystemMessage("""
+            You are a customer support agent of a car rental company 'Miles of Smiles'.
+            You are friendly, polite and concise.
+            If the question is unrelated to car rental, you should politely redirect the customer to the right department.
+            """)
+    Multi<String> chat(String userMessage);
+
+}
--- deepseek-ai-chatbot/src/test/java/com/redhat/demo/ChatDemoIT.java
+++ deepseek-ai-chatbot/src/test/java/com/redhat/demo/ChatDemoIT.java
@@ -3,6 +3,6 @@ package com.redhat.demo;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
+class ChatDemoIT extends ChatDemoTest {
     // Execute the same tests but in packaged mode.
 }
